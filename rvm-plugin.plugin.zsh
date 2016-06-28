ac2f098e09281c33d485968ea4c415c1c8f364f9
Joe Block <jpb@unixorn.net>
2016-06-28 16:19:39 -0700

    Add aliases, functions
    * If rvm isn't installed in the user's directory, add install-rvm function
    to make it easier to install without have to go to the website and track
    down the install instructions
    
    * Start adding useful rvm aliases

--- rvm-plugin.plugin.zsh
+++ rvm-plugin.plugin.zsh
@@ -7,4 +7,21 @@ if [[ -x $HOME/.rvm/scripts/rvm ]]; then
     export path=( "$path[@]" $RVM_DIR/bin ) # Add RVM to PATH for scripting
   fi
 
+  function rvm-update() {
+    rvm get head
+    rvm reload
+  }
+
+  alias gemsets='rvm gemset list'
+  alias rubies='rvm list rubies'
+
+else
+
+  function install-rvm() {
+    local rvm_installer
+    rvm_installer=$(mktemp /tmp/install_rvm.XXXXXX)
+    curl -sSL 'https://get.rvm.io' > "${rvm_installer}"
+    "${rvm_installer}" stable
+  }
+
 fi
